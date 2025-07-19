afeafa47d8d631c7254d83566f4cf3dd02e81c6e
an-af <ahnaf.fattah1@gmail.com>
2025-07-20 06:40:58 +0700

    initial commit

---
+++ format_string.py
@@ -0,0 +1,11 @@
+# Contoh [1] Pembulatan
+angka = 1.3928571428571428
+hasil_bulat = round(angka,2)
+print(hasil_bulat)
+
+# Contoh [2] Format menjadi String
+angka = 1.0 
+hasil_string = f"{angka:.2f}"
+print(hasil_string)
+
+# Contoh [3] Format menjadi String + pembulatan
---
+++ regexp.py
@@ -0,0 +1,21 @@
+# A RegEx, or Regular Expression, is a sequence of characters that forms a search pattern.
+
+# Dengan regex, aku bisa mencocokan karakter
+
+# -- Code --
+
+#1. Import Re, module untuk menjalankan operasi reguler expresion
+import re
+
+#2. variabel text, text yang akan di cek
+txt = "hello world somebody.."
+
+#3. Membuat notasi reguler expresion | ^
+result = re.search("^hell", txt)
+
+if result :
+    print("match")
+else:
+    print("no match")
+
+# -- Code -- #
